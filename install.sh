89c5c37b8c8e5815e296f4161a4e49179ad878d9
Jakov Krstulovic Opara <jakov.krstulovic@gmail.com>
2022-01-12 23:15:22 +0100

    install.sh init

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# git
+ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
+
+# shell
+symlink ~/.dotfiles/shell/.profile ~/.profile
+
+# shell - bash
+symlink ~/.dotfiles/shell/bash/.bashrc ~/.bashrc
