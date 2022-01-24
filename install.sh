f27a9fab184aafe6096e3d6423b33e38ca46151a
Jakov Krstulovic Opara <jakov.krstulovic@gmail.com>
2022-01-24 23:06:48 +0100

    sublime added

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
+symlink () {
+    ln -sf "$@"
+    # ln -sfT "$@"  # T in order to use file only
+}
+
 # git
-ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
+symlink ~/.dotfiles/git/.gitconfig ~/.gitconfig
 
 # shell
 symlink ~/.dotfiles/shell/.profile ~/.profile
 
 # shell - bash
 symlink ~/.dotfiles/shell/bash/.bashrc ~/.bashrc
+
+
+# sublime
+mkdir -p ~/.config/sublime-text-3/Packages/User
+symlink ~/.dotfiles/sublime/User/ ~/.config/sublime-text-3/Packages/
+
